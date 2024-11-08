f193a98eaf2f3b136c1512b77518f704e0a988fa
shinju4n <ju4nshin@gmail.com>
2024-11-08 17:19:16 +0900

    add : 배너 타입 추가

---
+++ selvers-admin-front/src/types/banner.type.ts
@@ -0,0 +1,14 @@
+export type BannerType = 0 | 1; // 0 : 메인 배너, 1 : 띠배너
+export type DateType = 0 | 1; // 0 : 등록일, 1 : 게시일
+export type BannerState = 1 | 2 | 3; // 1 : 노출, 2 : 노출 안함, 3 : 종료
+
+export type BannerItemType = {
+  id: number;
+  order: number;
+  title: string;
+  img: string;
+  created_at: string;
+  start_date: string;
+  end_date: string;
+  state: BannerState;
+};
