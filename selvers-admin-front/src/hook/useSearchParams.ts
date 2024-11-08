ddb79a2e130aca1b93b238c4514d53de1a7f29f8
shinju4n <ju4nshin@gmail.com>
2024-11-08 16:53:23 +0900

    feat: useQueryParams 구현

---
+++ selvers-admin-front/src/hook/useSearchParams.ts
@@ -0,0 +1,69 @@
+import { useSearchParams, useNavigate } from "react-router-dom";
+
+interface UseQueryParamsOptions {
+  isDirectPush?: boolean;
+  isReplace?: boolean;
+  preserveScroll?: boolean;
+}
+
+const useQueryParams = ({
+  isDirectPush = false,
+  isReplace = false,
+  preserveScroll = true,
+}: UseQueryParamsOptions = {}) => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const query = new URLSearchParams(searchParams);
+  const navigate = useNavigate();
+
+  const updateQueryAndNavigate = () => {
+    const queryString = `?${query.toString()}`;
+    if (isDirectPush) {
+      navigate(queryString, {
+        replace: isReplace,
+        preventScrollReset: !preserveScroll,
+      });
+    } else {
+      setSearchParams(queryString, {
+        replace: isReplace,
+        preventScrollReset: !preserveScroll,
+      });
+    }
+
+    return query.toString();
+  };
+
+  const get = (key: string) => {
+    return query.get(key);
+  };
+
+  const getAll = () => {
+    return Object.fromEntries(query.entries());
+  };
+
+  const set = (key: string, value?: string) => {
+    if (!value) {
+      query.delete(key);
+      return updateQueryAndNavigate();
+    }
+    query.set(key, value);
+    return updateQueryAndNavigate();
+  };
+
+  const setAll = (params: Record<string, string | string[]>) => {
+    Object.entries(params).forEach(([key, value]) => {
+      if (!value) {
+        query.delete(key);
+      } else if (
+        value !== "" &&
+        !(Array.isArray(value) && value.length === 0)
+      ) {
+        query.set(key, value.toString());
+      }
+    });
+    return updateQueryAndNavigate();
+  };
+
+  return { get, getAll, set, setAll, updateQueryAndNavigate };
+};
+
+export default useQueryParams;
