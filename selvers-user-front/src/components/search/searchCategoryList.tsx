cb644305f15113129d5c30fdd429eba5c6cc2694
shinju4n <ju4nshin@gmail.com>
2024-11-14 14:36:07 +0900

    feat : [사용자] 검색창 카테고리 전체 추가

--- selvers-user-front/src/components/search/searchCategoryList.tsx
+++ selvers-user-front/src/components/search/searchCategoryList.tsx
@@ -1,4 +1,5 @@
 import { useCategoryQuery } from "@/api/etc/category.query";
+import useQueryParams from "@/hook/useSearchParams";
 import { SearchCategory } from "@components/searchBarStyle";
 import { Link, useNavigate } from "react-router-dom";
 
@@ -8,17 +9,36 @@ interface SearchCategoryListProps {
 
 const SearchCategoryList = ({ handleCloseFilter }: SearchCategoryListProps) => {
   const navigate = useNavigate();
+  const queryParams = useQueryParams({
+    isReplace: true,
+    isSetPageFirst: true,
+  });
   const { data: categoryData } = useCategoryQuery();
 
   const onClickCategory = (categoryId: number) => {
     handleCloseFilter();
-    navigate(`/event-list?category=${categoryId}`);
+    if (window.location.pathname === "/event-list") {
+      queryParams.set("category", categoryId.toString());
+    } else {
+      navigate(`/event-list?category=${categoryId}`);
+    }
+  };
+
+  const onClickAll = () => {
+    if (window.location.pathname === "/event-list") {
+      queryParams.deleteQuery("category");
+    } else {
+      navigate("/event-list");
+    }
   };
 
   return (
     <SearchCategory>
       <p className="title">카테고리</p>
       <ul className="category">
+        <li onClick={onClickAll} style={{ cursor: "pointer" }}>
+          전체
+        </li>
         {categoryData?.map((category) => (
           <li
             key={category.id + category.name}
